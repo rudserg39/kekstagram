12e119dd45138d2dc58756edd72b79615d6cdf82
RudSerg <rudserg39@gmail.com>
2019-12-04 23:44:05 +0200

    Добавил перемещение пина

--- js/edit.js
+++ js/edit.js
@@ -4,34 +4,102 @@
 
 (function () {
 
-  var IMG_SCALE = {
-    min: '25%',
-    max: '100%'
+  var imgScale = {
+    MIN: '25%',
+    MAX: '100%'
   };
 
   var STEP = 25;
+
+  var filter = {
+    CHROME: 'grayscale',
+    SEPIA: 'sepia',
+    MARVIN: 'invert',
+    PHOBOS: 'blur',
+    HEAT: 'brightness'
+  };
+
+  var pinLineBoorders = {
+    LEFT: 0,
+    RIGHT: 453
+  };
+
 
   // Элементы изменения размера фото
   var scaleEditorContainer = document.querySelector('.img-upload__scale');
   var scaleButtonSmall = scaleEditorContainer.querySelector('.scale__control--smaller');
   var scaleButtonBig = scaleEditorContainer.querySelector('.scale__control--bigger');
   var scaleValue = scaleEditorContainer.querySelector('.scale__control--value');
-  var scaledImg = document.querySelector('.img-upload__preview').querySelector('img');
+  var image = document.querySelector('.img-upload__preview').querySelector('img');
 
 
-  // Функция изменения размера фото
+  // Элементы пина и фильтров
+  var pinContainer = document.querySelector('.img-upload__effect-level');
+  var pin = pinContainer.querySelector('.effect-level__pin');
+  var effectDepthLine = pinContainer.querySelector('.effect-level__depth');
+
+
+  // Изменения размера фото
   var scaleButtonClickHandler = function (evt) {
     if (evt.target === scaleButtonSmall) {
-      scaleValue.value = parseInt(scaleValue.value, 10) > parseInt(IMG_SCALE.min, 10) ? (parseInt(scaleValue.value, 10) - STEP) + '%' : IMG_SCALE.min;
+      scaleValue.value = parseInt(scaleValue.value, 10) > parseInt(imgScale.MIN, 10) ? (parseInt(scaleValue.value, 10) - STEP) + '%' : imgScale.MIN;
     }
 
     if (evt.target === scaleButtonBig) {
-      scaleValue.value = parseInt(scaleValue.value, 10) < parseInt(IMG_SCALE.max, 10) ? (parseInt(scaleValue.value, 10) + STEP) + '%' : IMG_SCALE.max;
+      scaleValue.value = parseInt(scaleValue.value, 10) < parseInt(imgScale.MAX, 10) ? (parseInt(scaleValue.value, 10) + STEP) + '%' : imgScale.MAX;
     }
 
-    scaledImg.setAttribute('style', 'transform: scale(' + parseInt(scaleValue.value, 10) / 100 + ')');
+    image.setAttribute('style', 'transform: scale(' + parseInt(scaleValue.value, 10) / 100 + ')');
   };
 
   scaleEditorContainer.addEventListener('click', scaleButtonClickHandler);
+
+
+  // Изменение фильтров
+  pin.addEventListener('mousedown', function (evt) {
+    evt.preventDefault();
+
+    var startCoords = {
+      x: evt.clientX
+    };
+
+    var mouseMoveHandler = function (moveEvt) {
+      moveEvt.preventDefault();
+
+      var shift = {
+        x: moveEvt.clientX - startCoords.x
+      };
+
+      startCoords = {
+        x: moveEvt.clientX
+      };
+
+
+      pin.style.left = pin.offsetLeft + shift.x + 'px';
+
+      effectDepthLine.style.width = pin.style.left;
+
+      if (pin.offsetLeft < pinLineBoorders.LEFT) {
+        pin.style.left = pinLineBoorders.LEFT + 'px';
+      }
+
+      if (pin.offsetLeft > pinLineBoorders.RIGHT) {
+        pin.style.left = pinLineBoorders.RIGHT + 'px';
+      }
+    };
+
+    var mouseUpHandler = function (upEvt) {
+      upEvt.preventDefault();
+
+      document.removeEventListener('mousemove', mouseMoveHandler);
+      document.removeEventListener('mouseup', mouseUpHandler);
+    };
+
+    document.addEventListener('mousemove', mouseMoveHandler);
+    document.addEventListener('mouseup', mouseUpHandler);
+  });
+
+  // image.style.filter = filter.SEPIA + '(100%)';
+
 
 })();
