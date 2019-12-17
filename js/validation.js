34cfbb2e7aab30820fef65378ec0d1e86764bd7d
rudserg39 <44981447+rudserg39@users.noreply.github.com>
2019-12-17 18:43:32 +0200

    Revert "Переработка валидации тегов"
    This reverts commit c6138c6db5d74e1b58d588e2ba24bef6624433ba.

--- js/validation.js
+++ js/validation.js
@@ -9,99 +9,63 @@
   tagsInput.style.outline = 'none';
 
 
-  
+  var checkFormValidity = function () {
 
-  // Исключение пустых строк
-  var filterTags = function (tag) {
-    return tag !== '';
+    // Деление строки на отдельные строки по пробелам
+    var tags = tagsInput.value.split(' ');
+
+    // Исключение пустых строк
+    var contentTags = tags.filter(function (tag) {
+      return tag !== '';
+    });
+
+
+    contentTags.forEach(function (tag) {
+
+      if (tag.toString().substr(0, 1) !== '#') {
+        tagsInput.style.border = '2px solid red';
+        tagsInput.setCustomValidity('Хэш-тег должен начинаться с "#"');
+      } else {
+        tagsInput.setCustomValidity('');
+        tagsInput.style.border = '2px solid green';
+      }
+
+
+      if (tag.toString().substr() === '#') {
+        tagsInput.style.border = '2px solid red';
+        tagsInput.setCustomValidity('Хэш-тег не может состоять только из одной решётки');
+      }
+
+
+      var repeats = contentTags.filter(function (checkedTag) {
+        return tag === checkedTag;
+      });
+
+      if (repeats.length > 1) {
+        tagsInput.style.border = '2px solid red';
+        tagsInput.setCustomValidity('Один и тот же хэш-тег не может быть использован дважды');
+      }
+
+      if (contentTags.length > 5) {
+        tagsInput.style.border = '2px solid red';
+        tagsInput.setCustomValidity('Нельзя указать больше пяти хэш-тегов');
+      }
+
+
+      if (tagsInput.value === '') {
+        tagsInput.style.border = 'none';
+      }
+
+    });
+
   };
 
-  var addTags = function () {
-    // Деление введённого текста на отдельные строки по пробелам
-    var tags = tagsInput.value.split(' ').filter(filterTags);
-    return tags;
-  };
-
-  
 
   tagsInput.addEventListener('input', function () {
-    var tagsArray = [];
-    tagsArray.push(addTags())
-    console.log(tagsArray);
-
-    tagsArray.forEach(function (tag) {
-      console.log(tag)
-    });
+    checkFormValidity();
   });
 
-
-
-//   // // Исключение пустых строк
-//   // var contentTags = tags.filter(function (tag) {
-//   //   return tag !== '';
-//   // });
-
-
-//     contentTags.forEach(function (tag) {
-
-//       // if (tag.toString().substr(0, 1) !== '#') {
-//       //   tagsInput.style.border = '2px solid red';
-//       //   tagsInput.setCustomValidity('Хэш-тег должен начинаться с "#"');
-//       // } else {
-//       //   tagsInput.setCustomValidity('');
-//       //   tagsInput.style.border = '2px solid green';
-//       // }
-
-
-//       // if (tag.toString().substr() === '#') {
-//       //   tagsInput.style.border = '2px solid red';
-//       //   tagsInput.setCustomValidity('Хэш-тег не может состоять только из одной решётки');
-//       // }
-
-
-//       // if (tagsInput.value.substr(0, 1) === '#' && tag.match(/#/g).length > 1) {
-//       //   tagsInput.style.border = '2px solid red';
-//       //   tagsInput.setCustomValidity('Хэш-теги разделяются пробелами');
-//       // }
-// console.log(contentTags);
-// console.log(tag);
-
-//       // var repeats = contentTags.filter(function (checkedTag) {
-//       //   return tag.toLowerCase() === checkedTag.toLowerCase();
-//       // });
-
-//       // if (repeats.length > 1) {
-//       //   tagsInput.style.border = '2px solid red';
-//       //   tagsInput.setCustomValidity('Один и тот же хэш-тег не может быть использован дважды');
-//       // }
-
-
-//       // if (contentTags.length > 5) {
-//       //   tagsInput.style.border = '2px solid red';
-//       //   tagsInput.setCustomValidity('Нельзя указать больше пяти хэш-тегов');
-//       // }
-
-//       // if (tag.length > 20) {
-//       //   tagsInput.style.border = '2px solid red';
-//       //   tagsInput.setCustomValidity('Максимальная длина одного хэш-тега 20 символов');
-//       // }
-
-
-//       // if (tagsInput.value === '') {
-//       //   tagsInput.style.border = 'none';
-//       // }
-
-//     });
-
-//   };
-
-
-//   tagsInput.addEventListener('input', function () {
-//     checkFormValidity();
-//   });
 
 })();
 
 // После очистки tagsInput.value при ошибке, всё равно выводится сообение о той же ошибке
-
-// Прии повторе тега 3 раза не выдаёт ошибку
